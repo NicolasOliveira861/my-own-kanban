604f6450c8e00f034f32e29d58034403f0696702
Nicolas Oliveira <nicksouza861@gmail.com>
2023-03-24 09:32:36 -0300

    feat: styled-components, eslint, theme, editor config

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    purple: {
+      primary: '#7C3AED',
+      secondary: '#E2D6FF',
+    },
+    black: {
+      text: '#403937',
+    },
+    bg: '#FBFAFF',
+  },
+};
